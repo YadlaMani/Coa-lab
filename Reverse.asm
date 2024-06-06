178be2debef51753761fe4a08122c2e15ce43b7e
Mani Yadla <133841094+YadlaMani@users.noreply.github.com>
2024-06-06 20:21:10 +0530

    Update Reverse.asm

--- Reverse.asm
+++ Reverse.asm
@@ -1,11 +1,11 @@
-title : "Reverse a string"
+title : "reverse  a string"
 .model small
 .stack 100h
 .data
-    text1 db "abcedf"
-    count dw 6
-    text2 db 6 dup(0)
-.code
+    text1 db "abcdef"
+    count dw 6 
+    text2 db 7 dup(?),'$'
+.code  
     mov ax,@data
     mov ds,ax
     mov es,ax
@@ -15,10 +15,15 @@ title : "Reverse a string"
     add di,count
     dec count
 again:mov al,text1[si]
-       mov text2[di],al
-       inc si
-       dec di
-       loop again
-       mov ah,4ch
-       int 21h
-       end
+      mov text2[di],al
+      inc si
+      dec di
+      loop again  
+      mov dx,offset text2  
+      mov ah,09h
+      
+      int 21h
+      
+      mov ah,4ch
+      int 21h
+      end
