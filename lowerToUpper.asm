78bb16ab6bd2614736fff800fea9856c79d9e9eb
Mani Yadla <133841094+YadlaMani@users.noreply.github.com>
2024-06-06 20:14:30 +0530

    Update lowerToUpper.asm

--- lowerToUpper.asm
+++ lowerToUpper.asm
@@ -1,20 +1,21 @@
-title : "lower case to upper case"
+title : "lower to upper"
 .model small
 .stack 100h
 .data
-    text db "abcdef"
+    text1 db "abcdef"
     count dw 6
-.code 
+.code
     mov ax,@data
     mov ds,ax
     mov es,ax
-    mov si,0
     mov cx,count
-again:mov al,text[si]
-      sub al,20h
-      mov text[si],al
-      inc si
-      loop again
-      mov ah,4ch
-      int 21h
-      end
+   
+    mov si,0
+again:mov al,text1[si]
+       sub al,20h
+       mov text1[si],al
+       inc si
+loop again
+mov ah,4ch
+int 21h
+end
