fec818a96d764d424455ec6f746a3717e84a96c6
Mani Yadla <133841094+YadlaMani@users.noreply.github.com>
2024-06-06 19:54:41 +0530

    Update signOfNumber.asm

--- signOfNumber.asm
+++ signOfNumber.asm
@@ -1,32 +1,33 @@
-title : "checking the sign"
+title : "sign of the number"
 .model small
 .stack 100h
 .data
-    num db 3h
-    positive db "The number is positibe$"
-    negative db "The number is negative$"
-    zero db "The number is zero$"  
+num db 0h
+positive dw "+ve number$"
+negative dw "-ve number$"
+zero dw "Zero$"
 .code
     mov ax,@data
     mov ds,ax
     xor ax,ax
     mov al,num
     cmp num,0
-    jg greater
-    jl lower
     jz zeros
+    jp greater
+    jl lower
+    
 greater:mov ah,09h
-       lea dx,positive
-       int 21h
-       jump end
+        lea dx,positive
+        int 21h
+        jmp end
 lower:mov ah,09h
       lea dx,negative
       int 21h
-      jump end
+      jmp end
 zeros:mov ah,09h
       lea dx,zero
       int 21h
-      jump end
+      jmp end
 end:mov ah,4ch
     int 21h
-  
+    
